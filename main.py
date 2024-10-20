82761b9262dd7c6adeb57227a97e53dde361fefc
zipsonic <rick@hail3.com>
2024-10-20 12:45:02 -0700

    Bookbot 9: count words

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 def main():
-    with open("books/frankenstein.txt") as f:
+    bookpath = "books/frankenstein.txt"
+    numwords = countwords(openbook(bookpath))
+    print(f"{numwords} found in {bookpath}")
+
+
+def openbook(bookpath):
+    with open(bookpath) as f:
         file_contents = f.read()
-    print(file_contents)
+    return file_contents
+
+def countwords(file_contents):
+    return len(file_contents.split())
 
 main()
+       
